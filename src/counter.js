2834c9e2a7f428a4af45aac1596d10df459e9a05
lftdev <torres.lf21@gmail.com>
2023-07-29 15:50:24 -0300

    Separate functions

--- src/counter.js
+++ src/counter.js
@@ -1,23 +1,19 @@
-export function setupCounter(container, sibling) {
-  const resultDisplay = document.createElement('div')
-  resultDisplay.classList.add('results-display')
-  container.insertBefore(resultDisplay, sibling)
+export function getInputs() {
   const getFoodAmount = () => parseInt(prompt('Enter food amount you will eat (g):'))
+  
   const getServingSize = () => parseInt(prompt('Enter serving size (g):'))
-  function getNutrientsAmount () {
+  
+  function getNutrientsKcalAmount () {
     const proteins = parseInt(prompt('Enter protein amount:')) * 4
     const carbs = parseInt(prompt('Enter carbs amount:')) * 4
     const fats = parseInt(prompt('Enter fats amount:')) * 9
     return { proteins, carbs, fats }
   }
-  const amount = getFoodAmount()
-  const serving = getServingSize()
-  const { proteins, carbs, fats } = getNutrientsAmount()
-  resultDisplay.innerHTML = `
-    <h2>Your Food Information</h2>
-    <p>Proteins calories: ${proteins}</p>
-    <p>Carbs calories: ${carbs}</p>
-    <p>Fats calories: ${fats}</p>
-    <p>TOTAL: ${((proteins + carbs + fats) * amount) / serving}</p>
-  `
+  
+  const foodAmount = getFoodAmount()
+  const servingSize = getServingSize()
+  const nutrientsKcal = getNutrientsKcalAmount()
+  return { foodAmount, servingSize, nutrientsKcal }
 }
+export const getTotalKcals = (foodAmount, servingSize, nutrientsKcal) =>
+  ((nutrientsKcal.proteins + nutrientsKcal.carbs + nutrientsKcal.fats) * foodAmount) / servingSize
--- src/main.js
+++ src/main.js
@@ -1,5 +1,26 @@
-import { setupCounter } from './counter.js'
+import { getTotalKcals, getInputs } from './counter.js'
 
 const main = document.querySelector('main')
-const btnCalc = document.getElementById('btnCalc')
-btnCalc.onclick = () => setupCounter(main, btnCalc)
+const form = document.querySelector('form')
+form.onsubmit = event => {
+  event.preventDefault()
+  const inputs = getInputs()
+  const result = getTotalKcals(inputs.foodAmount, inputs.servingSize, inputs.nutrientsKcal)
+  showResult(main, form, inputs.nutrientsKcal, result)
+}
+function showResult  (container, sibling, nutrients, result) {
+  const previous = document.querySelector('results-display')
+  if (previous) container.removeChild(previous)
+  const resultDisplay = document.createElement('div')
+  resultDisplay.classList.add('results-display')
+  container.insertBefore(resultDisplay, sibling)
+
+  const { proteins, carbs, fats } = nutrients
+  resultDisplay.innerHTML = `
+    <h2>Your Food Information</h2>
+    <p>Proteins calories: ${proteins}</p>
+    <p>Carbs calories: ${carbs}</p>
+    <p>Fats calories: ${fats}</p>
+    <p>TOTAL: ${result}</p>
+    `
+}
