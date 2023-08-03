d61aa3f59d300f2616be7355ed7080f5e983d259
lftdev <torres.lf21@gmail.com>
2023-08-03 16:14:04 -0300

    build: :wrench: Add linter

--- src/counter.js
+++ src/counter.js
@@ -1,15 +1,16 @@
-export function getInputs() {
+/* eslint-disable no-undef */
+export function getInputs () {
   const getFoodAmount = () => parseInt(prompt('Enter food amount you will eat (g):'))
-  
+
   const getServingSize = () => parseInt(prompt('Enter serving size (g):'))
-  
+
   function getNutrientsKcalAmount () {
     const proteins = parseInt(prompt('Enter protein amount:')) * 4
     const carbs = parseInt(prompt('Enter carbs amount:')) * 4
     const fats = parseInt(prompt('Enter fats amount:')) * 9
     return { proteins, carbs, fats }
   }
-  
+
   const foodAmount = getFoodAmount()
   const servingSize = getServingSize()
   const nutrientsKcal = getNutrientsKcalAmount()
