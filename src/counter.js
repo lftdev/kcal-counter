037188eb8c05377b2657305911f852c955f2031a
lftdev <torres.lf21@gmail.com>
2023-08-03 17:24:52 -0300

    refactor: :zap: Make concise code

--- src/counter.js
+++ src/counter.js
@@ -1,20 +1,14 @@
 /* eslint-disable no-undef */
 export function getInputs () {
-  const getFoodAmount = () => parseInt(prompt('Enter food amount you will eat (g):'))
-
-  const getServingSize = () => parseInt(prompt('Enter serving size (g):'))
-
-  function getNutrientsKcalAmount () {
-    const proteins = parseInt(prompt('Enter protein amount:')) * 4
-    const carbs = parseInt(prompt('Enter carbs amount:')) * 4
-    const fats = parseInt(prompt('Enter fats amount:')) * 9
-    return { proteins, carbs, fats }
+  return {
+    foodAmount: parseInt(prompt('Enter food amount you will eat (g):')),
+    servingSize: parseInt(prompt('Enter serving size (g):')),
+    macrosKcalAmount: {
+      proteins: parseInt(prompt('Enter protein amount:')) * 4,
+      carbs: parseInt(prompt('Enter carbs amount:')) * 4,
+      fats: parseInt(prompt('Enter fats amount:')) * 9
+    }
   }
-
-  const foodAmount = getFoodAmount()
-  const servingSize = getServingSize()
-  const nutrientsKcal = getNutrientsKcalAmount()
-  return { foodAmount, servingSize, nutrientsKcal }
 }
-export const getTotalKcals = (foodAmount, servingSize, nutrientsKcal) =>
-  ((nutrientsKcal.proteins + nutrientsKcal.carbs + nutrientsKcal.fats) * foodAmount) / servingSize
+export const getTotalKcals = (foodAmount, servingSize, macrosKcalAmount) =>
+  ((macrosKcalAmount.proteins + macrosKcalAmount.carbs + macrosKcalAmount.fats) * foodAmount) / servingSize
--- src/main.js
+++ src/main.js
@@ -5,12 +5,15 @@ const form = document.querySelector('form')
 form.onsubmit = event => {
   event.preventDefault()
   const inputs = getInputs()
-  const result = getTotalKcals(inputs.foodAmount, inputs.servingSize, inputs.nutrientsKcal)
-  showResult(main, form, inputs.nutrientsKcal, result)
+  const result = getTotalKcals(inputs.foodAmount, inputs.servingSize, inputs.macrosKcalAmount)
+  removePreviousOutput(main)
+  showResult(main, form, inputs.macrosKcalAmount, result)
 }
-function showResult  (container, sibling, nutrients, result) {
+function removePreviousOutput (container) {
   const previous = document.querySelector('.results-display')
   if (previous) container.removeChild(previous)
+}
+function showResult (container, sibling, nutrients, result) {
   const resultDisplay = document.createElement('div')
   resultDisplay.classList.add('results-display')
   container.insertBefore(resultDisplay, sibling)
@@ -21,6 +24,6 @@ function showResult  (container, sibling, nutrients, result) {
     <p>Proteins calories: ${proteins}</p>
     <p>Carbs calories: ${carbs}</p>
     <p>Fats calories: ${fats}</p>
-    <p>TOTAL: ${result}</p>
+    <h3>TOTAL: ${result}</h3>
     `
 }
