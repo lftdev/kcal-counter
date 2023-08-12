09ab2ba7430dd013eb2d327792b8b189dbc46ced
lftdev <torres.lf21@gmail.com>
2023-08-12 13:02:31 -0300

    build: :construction_worker: Add tailwindcss

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { getTotalKcals, getInputs } from './counter.js'
 import { removePreviousOutput, showResult } from './dom-manipulations.js'
+import './style.css'
 
 const main = document.querySelector('main')
 const form = document.querySelector('form')
